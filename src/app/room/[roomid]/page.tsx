1353574e8ee812f833a51b2a1d9ce26307217370
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-07-25 12:52:29 +0600

    Connecting rooms and securing cookies

--- src/app/room/[roomid]/page.tsx
+++ src/app/room/[roomid]/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useParams } from "next/navigation"
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 
 function formatTimeRemaining(seconds: number)
@@ -15,6 +15,10 @@ const Page = () => {
   const params = useParams()
   const roomId = params.roomid as string
 
+  
+
+  const [input, setInput] = useState("")
+  const inputRef = useRef<HTMLInputElement>(null)
 
   const [copyStatus, setCopyStatus] = useState("COPY")
   const [timeRemaining, setTimeRemaining] = useState<number | null>(null)
@@ -47,7 +51,25 @@ const Page = () => {
     </div>
     <button className="text-xs bg-zinc-800 hover:bg-red-600 px-3 py-1.5 rounded text-zinc-400 hover:text-white font-bold items-center gap-2 disabled:opacity-50 cursor-pointer"><span className="group-hover:animate-pulse">💣</span> DESTROY NOW</button>
   </header>
+  <div className="flex-1 overflow-y-auto p-4 space-y-4 scrollbar-thin">
+  </div>
+  <div className="p-4 border-t border-zinc-800 bg-zinc-900/30">
+    <div className="flex gap-4">
+      <div className="flex-1 relative group">
+        <span className="absolute left-4 top-1/2 -translate-y-1/2 text-green-500 animate-pulse">{">"}</span>
+        <input autoFocus value={input} onKeyDown={(e)=> {
+          if (e.key === "Enter" && input.trim()){
+            //TODO: send message
+            inputRef.current?.focus()
+          }
+        }} onChange={(e)=> setInput(e.target.value)} placeholder="Type message..." type="text" className="w-full bg-black border focus:border-zinc-700 focus:outline-none transition-colors placeholder:text-zinc-700 py-3 pl-8 pr-4 text-sm" />
+         
+      </div>
+      <button className="bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50 disabled:cursor-not-allowed">SEND</button>
+    </div>
+  </div>
   </main>
+
 }
 
 export default Page
