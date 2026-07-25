2504edb04d05f01fe30f88e24e734920a165b93f
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-07-25 13:12:52 +0600

    Complete authmiddleware

--- src/proxy.ts
+++ src/proxy.ts
@@ -15,6 +15,24 @@ export const proxy = async(req: NextRequest) => {
     return NextResponse.redirect(new URL("/?error=room-not-found", req.url))
   }
 
+  const existingToken = req.cookies.get("x-auth-token")?.value
+
+  //USER IS ALLOWED TO JOIN: Some one refreshing
+  if (existingToken && meta.connected.includes(existingToken)){
+    return NextResponse.next()
+  }
+
+
+  //USER IS NOT ALLOWED TO JOIN
+  //
+
+  if (meta.connected.length >= 2){
+    return NextResponse.redirect(new URL("/?error=room-full", req.url))
+  }
+
+
+
+
   const response = NextResponse.next()
 
   const token = nanoid()
@@ -25,7 +43,10 @@ export const proxy = async(req: NextRequest) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "strict"
+  })
 
+  await redis.hset(`meta:${roomId}`, {
+    connected: [...meta.connected, token]
   })
 
   return response
