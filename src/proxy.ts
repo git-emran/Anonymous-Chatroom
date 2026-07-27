a92834ff47c62c679848aa29097c789fb2f8afae
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-07-27 18:26:52 +0600

    refactor: rename middleware file to proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server"
 import { redis } from "./lib/redis"
 import { nanoid } from "nanoid"
 
-export const middleware = async(req: NextRequest) => {
+export const proxy = async(req: NextRequest) => {
   const pathname = req.nextUrl.pathname
   const roomMatch = pathname.match(/^\/room\/([^/]+)$/)
 
