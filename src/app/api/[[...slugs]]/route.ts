7dbffa21d3e5295be8d69371493ae847a744b8d1
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-07-27 17:59:32 +0600

    Project complete

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -16,7 +16,22 @@ const rooms =  new  Elysia({prefix: "/room"}).post("/create", async ()=> {
 
   await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS)
   return {roomId}
-})
+}).use(authMiddleware).get("/ttl", async({auth})=> {
+  const ttl = await redis.ttl(`meta:${auth.roomId}`)
+  return {ttl: ttl > 0 ? ttl : 0}
+
+}, {query: z.object({roomId: z.string()})}).delete("/", async({auth})=> {
+
+  await Promise.all([
+    redis.del(auth.roomId),
+    redis.del(`meta:${auth.roomId}`),
+    redis.del(`messages:${auth.roomId}`)
+
+  ])
+
+  await realtime.channel(auth.roomId).emit("chat.destroy", {isDestroyed: true})
+
+}, {query: z.object({roomId: z.string()})})
 
 const messages = new Elysia({prefix:"/messages"}).use(authMiddleware).post("/", async({body, auth})=> {
   const {sender, text} = body
@@ -67,4 +82,6 @@ const app = new Elysia({ prefix: '/api' }).use(rooms).use(messages)
 
 export const GET = app.fetch
 export const POST = app.fetch
+export const DELETE = app.fetch
+
 export type App = typeof app
